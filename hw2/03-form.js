133dd610f3f6b359722076f2a2b06c55df197e21
Alvin-PDX <iskender@pdx.edu>
2022-10-19 17:51:35 -0700

    api

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -5,17 +5,16 @@ let form = document.querySelector('form');
 let clear = document.getElementById('reset_button');
 
 form.addEventListener('submit', (event) => {
-  console.log('Saving value', form.elements);
   let inputs = form.elements;
   console.log(
     '~Form Submission~\n\nName: ' +
-      inputs['name'].value +
+      inputs.name.value +
       '\nEmail: ' +
-      inputs['email'].value
+      inputs.email.value
   );
-  if (inputs['feedback'].value == '') console.log('Feedback: None given.');
-  else console.log('Feedback: ' + inputs['feedback'].value);
-  if (inputs['newsletter'].checked)
+  if (inputs.feedback.value == '') console.log('Feedback: None given.');
+  else console.log('Feedback: ' + inputs.feedback.value);
+  if (inputs.newsletter.checked)
     console.log('Newsletter: Please register me for the newsletter.');
   else console.log('Newsletter: No thanks.');
   event.preventDefault();
@@ -23,8 +22,8 @@ form.addEventListener('submit', (event) => {
 
 clear.addEventListener('click', (event) => {
   let inputs = form.elements;
-  inputs['name'].value = '';
-  inputs['email'].value = '';
-  inputs['feedback'].value = '';
-  inputs['newsletter'].checked = false;
+  inputs.name.value = '';
+  inputs.email.value = '';
+  inputs.feedback.value = '';
+  inputs.newsletter.checked = false;
 });
